79880ba5e57e470f006406843567ec2802ce31b1
leonardogit <leocamargoslemes@hotmail.com>
2025-08-12 23:19:20 -0300

    avanco curso

--- QuerysCurso.sql
+++ QuerysCurso.sql
@@ -16,4 +16,26 @@ ALTER TABLE produtos
 ADD CONSTRAINT fk_produtos_marcas
 FOREIGN KEY (id_marca)
 REFERENCES marcas(id);
-     
+
+INSERT INTO marcas (
+    nome ,
+    site ,
+    telefone
+)
+VALUES 
+('Leonardo Camargos', 'leocamargos.com', '34998730005'),
+('Lucas Abreu', 'lucas.com', '34998730005');
+
+INSERT INTO produtos 
+(nome , preco , estoque , id_marca)
+VALUES 
+('Iphone 12 Pro max' , 4500.22 , 14 , 2);
+
+Alter TABLE produtos
+MODIFY nome VARCHAR(100);
+
+SELECT * from produtos
+
+SELECT id , nome FROM produtos;
+
+SELECT preco FROM produtos WHERE preco = 4500.22;
